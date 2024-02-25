afda601fb2103b651b1bc7a9a87bcd291ded838e
damevi <maciej.urlo@gmail.com>
2024-02-25 17:12:19 +0100

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting ""
 
-set -gx TERM xterm-256color
+fish_add_path /opt/homebrew/bin
+
+# set -gx TERM xterm-256color
 
 # aliases
 alias v nvim
@@ -8,3 +10,7 @@ alias ga "git add -A && git commit -m"
 alias gh "git push origin head"
 alias gr "grep --color -r"
 alias python python3
+
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
