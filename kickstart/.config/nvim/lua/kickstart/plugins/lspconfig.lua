a2b06a12284ae05e0775a87726880952d4b22bae
damevi <maciej.urlo@gmail.com>
2024-10-05 19:51:42 +0200

    config: add emmet ls

--- kickstart/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ kickstart/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -147,7 +147,9 @@ return {
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        -- pyright = {},
+        jedi_language_server = {},
+        ruff_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -157,7 +159,7 @@ return {
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
-
+        emmet_ls = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
